54de45427e669ef86783ea15412cff90ab321298
Lívia Dantas <liviadantas659@gmail.com>
2025-05-14 19:14:59 -0300

    Atualização da aplicação

--- src/ConversorMoedas.java
+++ src/ConversorMoedas.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import com.google.gson.Gson;
 
-public class ConversorMoedas extends Conversor {
+public class ConversorMoedas {
     public static void main(String[] args) throws IOException, InterruptedException {
 
           System.out.println("Olá, seja muito bem-vindo ao conversor de moedas!");
@@ -44,10 +44,9 @@ public class ConversorMoedas extends Conversor {
 
         System.out.println("Digite o valor que deseja converter:");
         double valorParaConverter = scanner.nextDouble();
-
+        
         scanner.close();
 
-        // Converter número em sigla
         String moedaBase = getCodigoMoeda(moedaBaseNumero);
         String moedaConvertida = getCodigoMoeda(moedaConvertidaNumero);
 
@@ -56,17 +55,11 @@ public class ConversorMoedas extends Conversor {
             return;
         }
 
-        // Cria o cliente HTTP
         HttpClient client = HttpClient.newHttpClient();
-
-        // Monta a URL com as moedas
         String url = "https://v6.exchangerate-api.com/v6/e47368b602e21b5295bbeaeb/pair/" + moedaBase + "/" + moedaConvertida;
-
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
-
-        // Envia a requisição e recebe a resposta
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         Gson gson = new Gson();
@@ -86,7 +79,7 @@ public class ConversorMoedas extends Conversor {
             case 4 -> "CLP";
             case 5 -> "COP";
             case 6 -> "USD";
-            default -> "EXIT"; // Para sair ou opção inválida
+            default -> "EXIT";
         };
     }
     
